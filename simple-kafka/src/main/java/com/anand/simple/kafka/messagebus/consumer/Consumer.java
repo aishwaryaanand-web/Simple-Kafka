3286c360169c4398787c40597cd257dc095371be
Aishwarya Anand <aakash.mit07@gmail.com>
2015-05-20 09:36:39 +0100

    Update Consumer.java

--- simple-kafka/src/main/java/com/anand/simple/kafka/messagebus/consumer/Consumer.java
+++ simple-kafka/src/main/java/com/anand/simple/kafka/messagebus/consumer/Consumer.java
@@ -1,6 +1,20 @@
 package com.anand.simple.kafka.messagebus.consumer;
-
+/**
+ * Consumer that can consume message of Type T from Apache Kafka, Message Type T will be decided by decoder 
+ * for consumer.
+ * 
+ * @author Aishwarya Anand
+ * @param <T>
+ *            - Type T is message type that Consumer will receive
+ */
 public interface Consumer<T> {
 
+	/**
+	 * Consumes message of a specific Type
+	 * 
+	 * @return  T
+	 *            - Type T is message type that Consumer will consume from
+	 * 		Apache Kafka
+	 */
 	T receive();
 }
